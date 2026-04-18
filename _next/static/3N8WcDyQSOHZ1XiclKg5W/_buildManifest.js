e174bbe027ee1f0c343c7a766b7a200cf587f0c7
infogalileo07-a11y <infogalileo07@gmail.com>
2026-04-18 13:24:55 +0900

    Initial GitHub Pages deployment

---
+++ _next/static/3N8WcDyQSOHZ1XiclKg5W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-04b27ebe69ac4c2d.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
